02be7fa8e974e15061c976bd9054688dc3c98cb9
shirone777 <799902937@qq.com>
2018-01-14 01:58:51 +0800

    1.987 fix

--- expansions/script/c22222.lua
+++ expansions/script/c22222.lua
@@ -46,7 +46,7 @@ function c22222.spfilter(c,e,tp)
 	return c:IsSetCard(0x20a2) and not c:IsCode(22222) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function c22222.spcon1(e,tp,eg,ep,ev,re,r,rp)
-	return eg:IsContains(e:GetHandler()) and rp~=tp and re:GetHandler():IsOnfield() and re:GetHandler():IsType(TYPE_MONSTER)
+	return eg:IsContains(e:GetHandler()) and rp~=tp and re:GetHandler():IsOnField() and re:GetHandler():IsType(TYPE_MONSTER)
 		and re:GetHandler():IsControlerCanBeChanged()
 end
 function c22222.spcon2(e,tp,eg,ep,ev,re,r,rp)
