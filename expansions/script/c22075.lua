79cc96507d5808dbc11d2bdd4ed67383f9823f47
shirone777 <799902937@qq.com>
2018-03-03 01:54:40 +0800

    f

--- expansions/script/c22075.lua
+++ expansions/script/c22075.lua
@@ -1,6 +1,5 @@
 --红魔✿秘白之女仆长 十六夜咲夜
 function c22075.initial_effect(c)
---
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_SPSUMMON_PROC)
@@ -9,7 +8,6 @@ function c22075.initial_effect(c)
 	e1:SetCountLimit(1,22075)
 	e1:SetCondition(c22075.con1)
 	c:RegisterEffect(e1)
---
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(22075,0))
 	e2:SetCategory(CATEGORY_COUNTER)
@@ -19,9 +17,7 @@ function c22075.initial_effect(c)
 	e2:SetTarget(c22075.tg2)
 	e2:SetOperation(c22075.op2)
 	c:RegisterEffect(e2)
---  
 end
---
 function c22075.con1_1(c)
 	return c:IsSetCard(0x814) and c:IsType(TYPE_MONSTER)
 end
@@ -34,7 +30,7 @@ function c22075.con1(e,c)
 end
 --
 function c22075.tfilter2(c)
-	return c:IsSetCard(0x813) and c:IsType(TYPE_SPELL) and c:IsAbleToHand()
+	return (c:IsSetCard(0x813) or c:GetOriginalCode()==(22100) or c:GetOriginalCode()==(22117)) and c:IsType(TYPE_SPELL) and c:IsAbleToHand()
 end
 function c22075.tg2(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(c22075.tfilter2,tp,LOCATION_DECK,0,1,nil) end
@@ -48,6 +44,5 @@ function c22075.op2(e,tp,eg,ep,ev,re,r,rp)
 	Duel.SendtoHand(g,nil,REASON_EFFECT)
 	Duel.ConfirmCards(1-tp,g)
 end
---
 
 
