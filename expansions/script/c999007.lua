ffd69763e92a57426f730382847a6b358dbd0d9e
oilloillo <32014537+oilloillo@users.noreply.github.com>
2018-03-20 12:42:17 +0800

    Update c999007.lua
    fix

--- expansions/script/c999007.lua
+++ expansions/script/c999007.lua
@@ -56,7 +56,7 @@ end
 
 function M.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if not e:GetHandler():IsOnField() then return false end
-	if eg:GetCount()==0 then return false end
+	if not eg or eg:GetCount()==0 then return false end
 	if chk==0 then 
 		local chkf = Duel.GetLocationCountFromEx(tp) > 0 and PLAYER_NONE or tp
 		local g = eg:Filter(M.spfliter, nil, tp)
